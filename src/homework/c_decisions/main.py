6ecd9d3db063a2432bd932b48103cc88956fd3a2
DuncanH26 <duncan.hemelstrand@g.austincc.edu>
2025-03-09 04:18:56 +0000

    Homework 2

--- src/homework/c_decisions/main.py
+++ src/homework/c_decisions/main.py
@@ -1,1 +1,18 @@
-#
+import decisions 
+
+def main():
+    options = int(input("Enter Options "))
+    total = int(input("Enter Total "))
+
+    Ratio = decisions.get_options_ratio(options, total)
+    Result = decisions.get_faculty_rating(Ratio)
+
+    print(options/total, Result)
+
+main()
+
+
+
+
+    
+    
--- tests/examples/c_decisions/tests_decisions.py
+++ tests/examples/c_decisions/tests_decisions.py
@@ -7,3 +7,9 @@ class Test_Config(unittest.TestCase):
     def test_configuration(self):
         self.assertEqual(True, test_config())
 
+    def test_logical_and_truth_table(self):
+        self.assertEqual(False, False and False)
+        self.assertEqual(False, False and True)
+        self.assertEqual(False, True and False)
+        self.assertEqual(True, True and True)
+
