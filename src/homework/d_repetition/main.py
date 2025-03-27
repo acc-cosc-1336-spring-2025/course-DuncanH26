2e75040d847b4bc145987cd618fe3062b27ef5b4
DuncanH26 <duncan.hemelstrand@g.austincc.edu>
2025-03-27 00:27:52 +0000

    Homework 3

--- src/homework/d_repetition/main.py
+++ src/homework/d_repetition/main.py
@@ -1,1 +1,42 @@
-#
+import repetition
+
+def main():
+    print('1-Factorial')
+    print('2-Sum odd numbers')
+    print('3-Exit')
+
+
+    option = input('Select an option: ')
+
+    if option == '1':
+        num_str = int(input('Enter a number 1-9: '))
+        num = int(num_str)
+        if 1 <= num <= 9:
+            result = repetition.get_factorial(num)
+            print(f'The Factorial of {num} is {result}')
+        else: 
+           print('Invalid Number')
+
+    elif option == '2':
+        num_str = int(input('Enter a number 1-99: '))
+        num = int(num_str)
+        if 1 <= num <= 99:
+            result = repetition.sum_odd_numbers(num)
+            print(f'The sum of odd numbers for {num} is {result}')
+                                                        
+        else: 
+         print('Invalid Number')
+
+    elif option == '3':
+       exit = input('Do you wish to exit? Y or N: ')
+       if exit == 'Y':
+          print('Program Exiting...')
+          
+       else: 
+             print('Try again')
+
+            
+    
+    
+                
+main()
--- src/homework/d_repetition/repetition.py
+++ src/homework/d_repetition/repetition.py
@@ -1,1 +1,44 @@
-#
+def get_factorial(num):
+    total = 0
+    
+    for val in range(0, num):
+        total = 1
+
+    for i in range(1, num +1):
+
+            total *= i
+    
+
+    return total
+
+def sum_odd_numbers(num):
+     
+     total = 0
+     i = 1
+     while i <= num:
+          total += i
+          i += 2
+
+     return total
+          
+
+def display_menu():
+     while True:
+         print("1-Factorial")
+         print("2-Sum odd numbers")
+         print("3-Exit")
+
+         option = input('Select an option: ')
+
+         if option == '1':
+              while True:
+                   num_str = int(input('Enter Number (1-9):'))
+                   num = int(num_str)
+                   if 1<= num <= 9:
+                        result = get_factorial(num)
+
+    
+
+
+   
+    
