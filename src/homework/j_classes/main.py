3cd3636937d9bd4596cc7e15f98bbb709bc2ea1d
DuncanH26 <duncan.hemelstrand@g.austincc.edu>
2025-05-04 04:36:15 +0000

    Homework 8

--- src/homework/j_classes/main.py
+++ src/homework/j_classes/main.py
@@ -1,1 +1,22 @@
 #
+import class_a
+
+def main():
+
+ d = class_a.die()
+
+ while True:
+  
+  d.roll()
+  print(d)
+
+  option = input('Do you wish to continue?(Y/N): ')
+  if option != 'Y':
+   
+   print('Program Exiting...')
+   break
+  
+main()
+
+
+
--- tests/homework/j_classes/tests_classes.py
+++ tests/homework/j_classes/tests_classes.py
@@ -1,1 +1,27 @@
 #
+import unittest
+from src.homework.j_classes import roll
+from src.homework.j_classes import get_rolled_value
+from src.homework.j_classes import __str__
+from src.homework.j_classes import die
+
+class Test_Config(unittest.TestCase):
+
+
+ def test_get_rolled_value(self):
+
+    d = die()
+    
+    d.roll
+    
+    roll_1 = d.get_rolled_value()
+    roll_2 = d.get_rolled_value()
+    roll_3 = d.get_rolled_value()
+
+
+    self.assertEqual(True, 1 <= roll_1 <=6)
+    self.assertEqual(True, 1 <= roll_2 <=6)
+    self.assertEqual(True, 1 <= roll_3 <=6)
+
+
+
